233799b2899b45d265033a1cdd0e02cde0de5225
erikson-code <erikson91249650@gmail.com>
2021-03-28 12:08:02 -0300

    Adding components

---
+++ components/celula.js
@@ -0,0 +1,16 @@
+
+const Celula = () => {
+
+    return (
+
+        <>
+            <h1>Casa</h1>
+
+        </>
+
+
+    )
+
+}
+
+export default Celula
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Head from 'next/head'
-
+import Celula from '../components/celula'
 
 export default function Home() {
   return (
-    <>
+    <div className="container bg-warning" style={{ height: "600px" }}>
 
 
-    </>
+      <div className="d-flex flex-row justify-content-center  bg-primary" style={{ height: "80%" }}>
+
+        <div className="col-sm-8   bg-secondary">
+
+            <Celula></Celula>
+
+        </div>
+
+
+
+
+      </div>
+
+    </div>
   )
 }
